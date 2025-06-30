09b2fe460cd9f4da1b7ec95daa9fc64db0c67143
fadhilhm <129720825+fadhilhm@users.noreply.github.com>
2025-07-01 00:21:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,29 +9,3 @@ myImage.addEventListener("click", () => {
         myImage.setAttribute("src", "images/ICTTTT.png");
     }
 });
-
-// username
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUsername() {
-    const myName = prompt("Please enter your name.");
-    if(!myName) {
-        setUsername();
-    } else {
-        localStorage.setItem("name", myName);
-        myHeading.textContent = `Selamat datang di Ekskul ICT!, ${myName}`;
-    }
-} 
-
-if (!localStorage.getItem("name")) {
-    setUsername();
-} else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Selamat datang di Ekskul ICT!, ${storedName}`;
-}
-
-
-myButton.addEventListener("click", () => {
-    setUsername();
-})
